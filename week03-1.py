634396cd6cf0182439d1cf56441ebfe3effa8235
JAINAMI0813 <jenny2n4685@gmail.com>
2025-03-09 19:38:48 +0800

    week03

--- week03-1.py
+++ week03-1.py
@@ -3,5 +3,4 @@
 
 class Solution:
     def coloredCells(self, n: int) -> int:
-        return n*n+(n-1)*(n-1)
-        #觀察題目的圖片(不看英文，不看中文)，頭轉45度，看到正方形
+        return n*n+(n-1)*(n-1)#觀察題目的圖片(不看英文，不看中文)，頭轉45度，看到正方形
